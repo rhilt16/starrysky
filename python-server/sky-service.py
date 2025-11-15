0fdb5a34273f2cd1c01cf685c677859918ad455c
rhilt16 <rhysjhilton16@gmail.com>
2025-11-15 10:47:35 +1000

    more

--- python-server/sky-service.py
+++ python-server/sky-service.py
@@ -1,11 +1,13 @@
 # skyfield_service.py
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from skyfield.api import Star, load, wgs84, N, E, W, S
 from skyfield.data import hipparcos
 from skyfield.magnitudelib import planetary_magnitude
 from math import sin, cos, radians, degrees
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 class CelestialBody:
     def __init__(self, name, magnitude, position):
@@ -14,24 +16,21 @@ class CelestialBody:
         self.position = position
 
 celestial_bodies = []
+old_magnitude = 0.0
 
-
+displayed_objects = []
 
 with load.open("hip_main.dat") as f:
     df = hipparcos.load_dataframe(f)
 print(f"Loaded {len(df)} stars from Hipparcos catalog.")
 
 df = df[df['ra_degrees'].notnull()]
-print(df.keys())
-print(df.head())
-print(f"Stars brighter than a magnitude of 10.0: {len(df[df['magnitude'] <= 10.0])}")
 
-# Load ephemeris once → much faster
 ts = load.timescale()
 planets = load('de405.bsp')
 sun = planets['sun']
 earth = planets['earth']
-
+observer = earth + wgs84.latlon(-33.8688 * N, 151.2093 * W, elevation_m=43)
 
 targets = {
     "mercury": planets["mercury"],
@@ -43,8 +42,21 @@ targets = {
     "neptune": planets["neptune barycenter"]
 }
 
+
+# So far, sky checks for magnitude and loops through all
+
+# Celestial bodies are stored globally
+
+# New endpoint to change magnitude threshold
+
+# New endpoint to change location
+
+
 @app.get("/sky")
 def get_sky(min_magnitude: float = 6.0, time: str = None, loc_N: float = -33.8688, loc_W: float = 151.2093, loc_elev: float = 43):
+    global celestial_bodies
+    global observer
+    
     if(request.args.get('time')):
         time = request.args.get('time')
     if(request.args.get('min_magnitude')):
@@ -57,30 +69,39 @@ def get_sky(min_magnitude: float = 6.0, time: str = None, loc_N: float = -33.868
         loc_elev = float(request.args.get('loc_elev'))
     
     t=ts.now()
-
+    
     observer = earth + wgs84.latlon(loc_N * N, loc_W * W, elevation_m=loc_elev)  # Example: Sydney, Australia
     stars_df = df[df['magnitude'] <= min_magnitude]
+    stars_df = stars_df.sort_values(by='magnitude')
 
     # Iterate through stars and compute positions
     count = 0
+    added_count = 0
+  
     for hip_id, star_data in stars_df.iterrows():
         star = Star(ra_hours=star_data['ra_degrees'] / 15.0,
                     dec_degrees=star_data['dec_degrees'])
-        
+        name = f"HIP {hip_id}"
         
         astrometric = observer.at(t).observe(star)
         alt, az, distance = astrometric.apparent().altaz()
         if(alt.degrees < 0):
+            if(any(body.name == name.capitalize() for body in celestial_bodies)):
+                celestial_bodies = [body for body in celestial_bodies if body.name != name]
             continue  # Skip bodies below the horizon
-        #print(f"alt: {alt}, az: {az}, distance: {distance} \n")
+
         count += 1
+        if(celestial_bodies):
+            if(any(body.name == name for body in celestial_bodies)):
+                continue  # Skip if already in the list
+        
         newBody = CelestialBody(
             name=f"HIP {hip_id}",
             magnitude=star_data['magnitude'],
             position={"alt": alt.degrees, "az": az.degrees, "distance_au": distance.au}
-        )
-        if(newBody not in celestial_bodies):
-            celestial_bodies.append(newBody)
+        )       
+        celestial_bodies.append(newBody)
+        added_count += 1
 
     for name, body in targets.items():
         # Calculate magnitude as seen from Earth
@@ -90,7 +111,15 @@ def get_sky(min_magnitude: float = 6.0, time: str = None, loc_N: float = -33.868
         alt, az, distance = astrometric.apparent().altaz()
         #print(f"alt: {alt}, az: {az}, distance: {distance} \n")
         if(alt.degrees < 0):
+            if(any(body.name == name for body in celestial_bodies)):
+                celestial_bodies = [body for body in celestial_bodies if body.name != name]
             continue  # Skip bodies below the horizon
+
+        count += 1
+        if(celestial_bodies):
+            if(any(body.name == name.capitalize() for body in celestial_bodies)):
+                continue  # Skip if already in the list
+
         newBody = CelestialBody(
             name=name.capitalize(),
             magnitude=magnitude,
@@ -98,9 +127,133 @@ def get_sky(min_magnitude: float = 6.0, time: str = None, loc_N: float = -33.868
         )
         if(newBody not in celestial_bodies):
             celestial_bodies.append(newBody)
-        count += 1
+        
+        added_count += 1
 
-    return jsonify({"count": count, "message": "This endpoint is under construction.", "names": [body.name for body in celestial_bodies], "bodies": [{"name": body.name, "magnitude": body.magnitude, "position": body.position} for body in celestial_bodies]})
+    celestial_bodies.sort(key=lambda body: body.magnitude)
+    return jsonify({"count": count,  "added_count": added_count, "message": "This endpoint is under construction."})
+
+@app.get("/test")
+def get_stars_count(min_magnitude: float = 6.0, time: str = None, loc_N: float = -33.8688, loc_W: float = 151.2093, loc_elev: float = 43):
+    if(request.args.get('time')):
+        time = request.args.get('time')
+    if(request.args.get('min_magnitude')):
+        min_magnitude = float(request.args.get('min_magnitude'))
+    
+    stars_df = df[df['magnitude'] <= min_magnitude]
+    count = len(stars_df)
+    return jsonify({"count": count})
+
+@app.get("/celestial-bodies")
+def get_celestial_bodies():
+    global celestial_bodies
+    return jsonify({"names": [body.name for body in celestial_bodies], "bodies": [{"name": body.name, "magnitude": body.magnitude, "position": body.position} for body in celestial_bodies]})
+
+# Greater Magnitude Threshold (Fainter Objects)
+@app.get("/zoom-out")
+def zoom_out(new_magnitude: float=0.0):
+    global celestial_bodies
+    global observer
+    global old_magnitude
+
+    if(request.args.get('new_magnitude')):
+        new_magnitude = float(request.args.get('new_magnitude'))
+    else:
+        response = make_response(jsonify({"message": "new_magnitude parameter is required."}), 400)
+        return response
+    
+    if(old_magnitude >= new_magnitude):
+        response = make_response(jsonify({"message": "new_magnitude must be greater than old_magnitude."}), 400)
+        return response
+    
+    t=ts.now()
+    stars_df = df[(df['magnitude'] <= new_magnitude) & (df['magnitude'] > old_magnitude)]
+    stars_df = stars_df.sort_values(by='magnitude')
+    old_magnitude = new_magnitude
+
+     # Iterate through stars and compute positions
+    count = celestial_bodies.__len__()
+    added_count = 0
+  
+    for hip_id, star_data in stars_df.iterrows():
+        star = Star(ra_hours=star_data['ra_degrees'] / 15.0,
+                    dec_degrees=star_data['dec_degrees'])
+        name = f"HIP {hip_id}"
+        
+        astrometric = observer.at(t).observe(star)
+        alt, az, distance = astrometric.apparent().altaz()
+        if(alt.degrees < 0):
+            if(any(body.name == name.capitalize() for body in celestial_bodies)):
+                celestial_bodies = [body for body in celestial_bodies if body.name != name]
+            continue  # Skip bodies below the horizon
+
+        count += 1
+        if(celestial_bodies):
+            if(any(body.name == name for body in celestial_bodies)):
+                continue  # Skip if already in the list
+        
+        newBody = CelestialBody(
+            name=f"HIP {hip_id}",
+            magnitude=star_data['magnitude'],
+            position={"alt": alt.degrees, "az": az.degrees, "distance_au": distance.au}
+        )       
+        celestial_bodies.append(newBody)
+        added_count += 1
+
+    for name, body in targets.items():
+        # Calculate magnitude as seen from Earth
+        astrometric = observer.at(t).observe(body)
+        # Compute the magnitude
+        magnitude = float(planetary_magnitude(astrometric))
+        alt, az, distance = astrometric.apparent().altaz()
+        #print(f"alt: {alt}, az: {az}, distance: {distance} \n")
+        if(alt.degrees < 0):
+            if(any(body.name == name for body in celestial_bodies)):
+                celestial_bodies = [body for body in celestial_bodies if body.name != name]
+            continue  # Skip bodies below the horizon
+
+        count += 1
+        if(celestial_bodies):
+            if(any(body.name == name.capitalize() for body in celestial_bodies)):
+                continue  # Skip if already in the list
+
+        newBody = CelestialBody(
+            name=name.capitalize(),
+            magnitude=magnitude,
+            position={"alt": alt.degrees, "az": az.degrees, "distance_au": distance.au}
+        )
+        if(newBody not in celestial_bodies):
+            celestial_bodies.append(newBody)
+        
+        added_count += 1
+
+
+    celestial_bodies.sort(key=lambda body: body.magnitude)
+    return jsonify({"count": count,  "added_count": added_count, "message": "This endpoint is under construction."})
+
+@app.get("/zoom-in")
+def zoom_in(new_magnitude: float=0.0):
+    global celestial_bodies
+    global old_magnitude
+
+    if(request.args.get('new_magnitude')):
+        new_magnitude = float(request.args.get('new_magnitude'))
+    else:
+        response = make_response(jsonify({"message": "new_magnitude parameter is required."}), 400)
+        return response
+    
+    if(old_magnitude <= new_magnitude):
+        response = make_response(jsonify({"message": "new_magnitude must be less than old_magnitude."}), 400)
+        return response
+    
+    old_count = celestial_bodies.__len__()
+    # Logic to remove celestial bodies with magnitude greater than new_magnitude
+    celestial_bodies = [body for body in celestial_bodies if body.magnitude <= new_magnitude]
+
+    new_count = celestial_bodies.__len__()
+    old_magnitude = new_magnitude
+
+    return jsonify({"message": "Narrowed in.", "old_count": old_count, "new_count": new_count})
 
 
 if __name__ == "__main__":
--- starry-sky-frontend/src/App.tsx
+++ starry-sky-frontend/src/App.tsx
@@ -1,80 +1,141 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import axios from 'axios'
+import axios, { Axios } from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [position, setPosition] = useState({ longitude: '', latitude: '' })
+  const [position, setPosition] = useState({ longitude: '', latitude: '', magnitude: '', elevation: '' })
 
-  const handlePositionSubmit = (e: React.FormEvent) => {
+  async function handlePositionSubmit(e: React.FormEvent) {
     e.preventDefault()
-    console.log('Submitted position:', position)
-    // Here you can add logic to handle the submitted position
+    let longitude: number = parseFloat(position.longitude);
+    let latitude: number = parseFloat(position.latitude);
+    let magnitude: number = parseFloat(position.magnitude);
+    let elevation: number = parseFloat(position.elevation);
 
+    const url: string = `http://127.0.0.1:5001/sky?`;
+    let queryParams: string[] = [];
+
+    if(!isNaN(longitude)) {
+      queryParams.push(`loc_N=${longitude}`);
+    }
+    if(!isNaN(latitude)) {
+      queryParams.push(`loc_W=${latitude}`);
+    }
+    if(!isNaN(magnitude)) {
+      queryParams.push(`min_magnitude=${magnitude}`);
+    }
+    if(!isNaN(elevation)) {
+      queryParams.push(`elevation=${elevation}`);
+    }
+
+    const fullUrl: string = url + queryParams.join('&');
+    console.log('Request URL:', fullUrl);
+    const response = await axios.get(fullUrl);
+
+    if(!response.data){
+      console.error('No data received from server');
+      return;
+    }
+    alert('Received ' + response.data.names.length + ' celestial bodies from server, ' + "and added " + response.data.added_count + " new bodies.");
+    console.log('Response data:', response.data);
   }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="test-form container mt-4">
         <form onSubmit={handlePositionSubmit} className="p-3 border rounded bg-light">
+          <table className="table p-4 w-50 mx-auto" style={{marginLeft: '20%', fontSize: '20px'}}>
+            <thead>
+              <tr style={{height: '40px', width: '200px'}}>
+                <th scope="col">Input Name</th>
+                <th scope="col">Input Value</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <tr style={{height: '40px'}}>
+                <td style={{width: '100px'}}>
+                  <label className="form-label">Longitude N</label>
+                </td>
+                <td style={{width: '60px', paddingLeft: '20px'}}>
+                  <input
+                    type="text"
+                    style={{fontSize: '20px'}}
+                    className="form-control"
+                    placeholder="Longitude N"
+                    value={position.longitude}
+                    onChange={e => setPosition({ ...position, longitude: e.target.value })}
+                    required
+                  />
+                </td>
+              </tr>
+
+              <tr style={{height: '40px'}}>
+                <td style={{width: '100px'}}>
+                  <label className="form-label">Latitude W</label>
+                </td>
+                <td style={{width: '60px', paddingLeft: '20px'}}>
+                  <input
+                    type="text"
+                    style={{fontSize: '20px'}}
+                    className="form-control"
+                    placeholder="Latitude W"
+                    value={position.latitude}
+                    onChange={e => setPosition({ ...position, latitude: e.target.value })}
+                    required
+                  />
+                </td>
+              </tr>
+
+              <tr style={{height: '40px'}}>
+                <td style={{width: '100px'}}>
+                  <label className="form-label">Elevation (M)</label>
+                </td>
+                <td style={{width: '60px', paddingLeft: '20px'}}>
+                  <input
+                    type="text"
+                    style={{fontSize: '20px'}}
+                    className="form-control"
+                    placeholder="20.0"
+                    value={position.elevation}
+                    onChange={e => setPosition({ ...position, elevation: e.target.value })}
+                    required
+                  />
+                </td>
+              </tr>
+
+              <tr style={{height: '40px'}}>
+                <td style={{width: '100px'}}>
+                  <label className="form-label">Magnitude</label>
+                </td>
+                <td style={{width: '60px', paddingLeft: '20px'}}>
+                  <input
+                    type="text"
+                    style={{fontSize: '20px'}}
+                    className="form-control"
+                    placeholder="20.0"
+                    value={position.magnitude}
+                    onChange={e => setPosition({ ...position, magnitude: e.target.value })}
+                    required
+                  />
+                </td>
+              </tr>
+            </tbody>
+            <div className="text-center mt-4">
+              <button type="submit" className="btn btn-primary px-4">
+                Submit
+              </button>
+            </div>
+          </table>
+
           
-          <div className="mb-3 row align-items-center">
-            <label className="col-sm-4 col-form-label text-sm-end">Longitude N</label>
-            <div className="col-sm-8">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Longitude N"
-                value={position.longitude}
-                onChange={e => setPosition({ ...position, longitude: e.target.value })}
-              />
-            </div>
-          </div>
-
-          <div className="mb-3 row align-items-center">
-            <label className="col-sm-4 col-form-label text-sm-end">Latitude E</label>
-            <div className="col-sm-8">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Latitude E"
-                value={position.latitude}
-                onChange={e => setPosition({ ...position, latitude: e.target.value })}
-              />
-            </div>
-          </div>
-
-          <div className="text-center mt-4">
-            <button type="submit" className="btn btn-primary px-4">
-              Submit
-            </button>
-          </div>
 
         </form>
       </div>
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
